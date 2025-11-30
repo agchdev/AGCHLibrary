b95828779680bdd9edc48d5b3b6fba4f1c580f89
agchdev <alex.chinchi02@gmail.com>
2025-11-30 11:07:30 +0100

    nuwvo

--- dist/components/ui/index.js
+++
@@ -1,5 +0,0 @@
-export { default as Button } from "./Button";
-export { default as Badge } from "./Badge";
-export { default as ScrollStack } from "./ScrollStack";
-export { default as BubbleMenu } from "./BubbleMenu";
-export { default as AnimatedList } from "./AnimatedList";
