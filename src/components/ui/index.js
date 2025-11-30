9ce7b21dfb3b8feb27cce25bf7c27c2b9dbeb8e3
agchdev <alex.chinchi02@gmail.com>
2025-11-30 01:01:32 +0100

    nuwvo

--- src/components/ui/index.js
+++ src/components/ui/index.js
@@ -3,5 +3,6 @@ export { default as Badge } from "./Badge";
 export { default as Card } from "./Card";
 export { default as ScrollStack } from "./ScrollStack";
 export { default as BubbleMenu } from "./BubbleMenu";
+export { default as AnimatedList } from "./AnimatedList";
 
 export * from "./hooks"; // esto exporta useLenis
